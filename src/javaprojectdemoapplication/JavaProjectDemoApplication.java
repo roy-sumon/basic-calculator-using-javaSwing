d815c3faa3f4e0b3f9223e4e8b4d957c691dee0c
Sumon Roy <103359279+roy-sumon@users.noreply.github.com>
2024-11-28 22:49:44 +0600

    first comment

---
+++ src/javaprojectdemoapplication/JavaProjectDemoApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaprojectdemoapplication;
+
+/**
+ *
+ * @author 88017
+ */
+public class JavaProjectDemoApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FristJFrame.main(args);
+    }
+    
+}
